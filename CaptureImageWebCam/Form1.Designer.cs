1a55a7e892e65ca806eeb0ae5dedb6c016d1d8fc
Jakkapan <Jakkapan.attala@gmail.com>
2023-01-11 10:59:58 +0700

    Testing

--- CaptureImageWebCam/Form1.Designer.cs
+++ CaptureImageWebCam/Form1.Designer.cs
@@ -29,12 +29,159 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.btnConn = new System.Windows.Forms.Button();
+            this.rManual = new System.Windows.Forms.RadioButton();
+            this.rAuto = new System.Windows.Forms.RadioButton();
+            this.btnCapture = new System.Windows.Forms.Button();
+            this.timerVideo = new System.Windows.Forms.Timer(this.components);
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(13, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(775, 376);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(13, 404);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // btnConn
+            // 
+            this.btnConn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnConn.Location = new System.Drawing.Point(141, 404);
+            this.btnConn.Name = "btnConn";
+            this.btnConn.Size = new System.Drawing.Size(75, 23);
+            this.btnConn.TabIndex = 4;
+            this.btnConn.Text = "Connect";
+            this.btnConn.UseVisualStyleBackColor = true;
+            this.btnConn.Click += new System.EventHandler(this.btnConn_Click);
+            // 
+            // rManual
+            // 
+            this.rManual.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.rManual.AutoSize = true;
+            this.rManual.Location = new System.Drawing.Point(592, 405);
+            this.rManual.Name = "rManual";
+            this.rManual.Size = new System.Drawing.Size(60, 17);
+            this.rManual.TabIndex = 5;
+            this.rManual.TabStop = true;
+            this.rManual.Text = "Manual";
+            this.rManual.UseVisualStyleBackColor = true;
+            // 
+            // rAuto
+            // 
+            this.rAuto.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.rAuto.AutoSize = true;
+            this.rAuto.Location = new System.Drawing.Point(539, 405);
+            this.rAuto.Name = "rAuto";
+            this.rAuto.Size = new System.Drawing.Size(47, 17);
+            this.rAuto.TabIndex = 5;
+            this.rAuto.TabStop = true;
+            this.rAuto.Text = "Auto";
+            this.rAuto.UseVisualStyleBackColor = true;
+            // 
+            // btnCapture
+            // 
+            this.btnCapture.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCapture.Location = new System.Drawing.Point(680, 401);
+            this.btnCapture.Name = "btnCapture";
+            this.btnCapture.Size = new System.Drawing.Size(105, 23);
+            this.btnCapture.TabIndex = 6;
+            this.btnCapture.Text = "Capture";
+            this.btnCapture.UseVisualStyleBackColor = true;
+            this.btnCapture.Click += new System.EventHandler(this.btnCapture_Click);
+            // 
+            // timerVideo
+            // 
+            this.timerVideo.Interval = 50;
+            this.timerVideo.Tick += new System.EventHandler(this.timerVideo_Tick);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(13, 17);
+            this.toolStripStatusLabel1.Text = "1";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(13, 17);
+            this.toolStripStatusLabel2.Text = "2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCapture);
+            this.Controls.Add(this.rAuto);
+            this.Controls.Add(this.rManual);
+            this.Controls.Add(this.btnConn);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Button btnConn;
+        private System.Windows.Forms.RadioButton rManual;
+        private System.Windows.Forms.RadioButton rAuto;
+        private System.Windows.Forms.Button btnCapture;
+        private System.Windows.Forms.Timer timerVideo;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
     }
 }
 
--- CaptureImageWebCam/Form1.cs
+++ CaptureImageWebCam/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using DirectShowLib;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,154 @@ namespace CaptureImageWebCam
         public Form1()
         {
             InitializeComponent();
+        }
+        private string _path = @"./system";
+        private OpenCvSharp.VideoCapture capture;
+        private bool IsCapture;
+        private Image Image = null;
+
+        private bool statebtnCaptue = false;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Get drive web cam to list
+            var videoDevices = new List<DsDevice>(DsDevice.GetDevicesOfCat(FilterCategory.VideoInputDevice));
+
+            foreach (var device in videoDevices)
+            {
+                comboBox1.Items.Add(device.Name);
+            }
+            if (comboBox1.Items.Count > 0)
+            {
+                comboBox1.SelectedIndex = 0;
+            }
+
+
+            // Chack path
+            if (!Directory.Exists(_path))
+            {
+                Directory.CreateDirectory(_path);
+            }
+        }
+
+        private void btnConn_Click(object sender, EventArgs e)
+        {
+            int deviceIndex = comboBox1.SelectedIndex;
+            capture = new OpenCvSharp.VideoCapture(deviceIndex);
+            capture.Open(deviceIndex);
+            SetSizeImage();
+
+            timerVideo.Start();
+        }
+        private void SetSizeImage(string name = "HD")
+        {
+            int _height = 1080;
+            int _width = 1920;
+            switch (name.ToUpper())
+            {
+                case "HD":
+                    _height = 720;
+                    _width = 1280;
+                    break;
+                case "FULLHD":
+                    _height = 1080;
+                    _width = 1920;
+                    break;
+            }
+
+            capture.Set(OpenCvSharp.VideoCaptureProperties.FrameHeight, _height);
+            capture.Set(OpenCvSharp.VideoCaptureProperties.FrameWidth, _width);
+        }
+        private void timerVideo_Tick(object sender, EventArgs e)
+        {
+            if (capture.IsOpened() && capture != null)
+            {
+                try
+                {
+                    using (OpenCvSharp.Mat frame = new OpenCvSharp.Mat())
+                    {
+                        capture.Read(frame);
+                        if (!frame.Empty())
+                        {
+                            pictureBox1.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(frame);
+                            if (statebtnCaptue)
+                            {
+                                using (Image image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(frame))
+                                {
+                                    saveFileImage(image);
+                                }
+                            }
+                           
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    pictureBox1.Image = null;
+                }
+                finally
+                {
+                }
+            }
+        }
+
+        private void btnCapture_Click(object sender, EventArgs e)
+        {
+            if (!capture.IsOpened() || capture == null)
+                return;
+
+
+            if (rAuto.Checked && statebtnCaptue == false)
+            {
+                statebtnCaptue = true;
+                // Set Text 
+                btnCapture.Text = "Run..";
+
+                toolStripStatusLabel1.Text = "Save...";
+               
+            }
+            else
+            if (rAuto.Checked && statebtnCaptue)
+            {
+                statebtnCaptue = false;
+                btnCapture.Text = "Capture";
+                toolStripStatusLabel1.Text = "Redy";
+            }
+            else if (rManual.Checked)
+            {
+                if (pictureBox1.Image != null)
+                {
+                    saveFileImage(pictureBox1.Image);
+                    toolStripStatusLabel1.Text = "Save";
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (capture != null)
+            {
+                capture.Dispose();
+                pictureBox1.Image = null;
+            }
+
+            timerVideo.Stop();
+        }
+
+        private String saveFileImage(Image image)
+        {
+            string filename = Guid.NewGuid().ToString();
+            string _path = Path.Combine(this._path, "images");
+            // Chack path
+            if (!Directory.Exists(_path))
+            {
+                Directory.CreateDirectory(_path);
+            }
+            _path = Path.Combine(this._path, "images",filename+".jpg");
+            image.Save(_path);
+
+            toolStripStatusLabel2.Text = filename + ".jpg";
+            return filename + "jpg";
         }
     }
 }
